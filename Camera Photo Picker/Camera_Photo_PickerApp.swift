fc1a5c2400c483bb35c528a53a70ac14ff538278
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-11-14 09:22:05 -0600

    Initial Commit

---
+++ Camera Photo Picker/Camera_Photo_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Photo_PickerApp.swift
+//  Camera Photo Picker
+//
+//  Created by Randy McKown on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Photo_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
